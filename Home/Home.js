2f22acb04fb0a30ea982e725f114122a7ca4f169
JeffLatham <jeff.r.latham@gmail.com>
2015-07-30 18:15:15 -0500

    Carousel sizing improvements

--- Home/Home.js
+++ Home/Home.js
@@ -1,14 +1,7 @@
 // JavaScript Document
 
 var main = function() {
-	// carousel sizing
-	var carouselWidth = $('.carousel').width();
-	var carouselHeight = carouselWidth*0.37;
-	$('.carousel').height(carouselHeight);
 	
-	// carousel image sizing
-	$('.item>img').width('100%');
-	$('.item>img').height(carouselHeight);
 }
 
 $(document).ready(main);
