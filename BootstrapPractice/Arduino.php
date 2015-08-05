6652f665048f6101e5573cd13139a69e24ffe592
JeffLatham <jeff.r.latham@gmail.com>
2015-08-05 10:26:39 -0500

    commit

---
+++ BootstrapPractice/Arduino.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+	<!-- Meta -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.89">
+    
+    <title>Arduino</title>
+    
+    <!-- CSS -->
+    <link rel="stylesheet" href="../bootstrap/bootstrap-3.3.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    
+    <!-- JavaScript -->
+    <script src="../jQuery/jquery.js"></script>
+    <script src="../bootstrap/bootstrap-3.3.2-dist/js/bootstrap.min.js"></script>
+    <script src="test.js"></script>
+    
+    <!-- PHP -->
+	<?php
+        include '';
+    ?>
+</head>
+
+<body>
+    
+    <div class="page">
+    	<div class="section">
+            
+            <a href="http://192.168.1.6/?test1" class="btn btn-info">Test 1</a>
+            
+            <a href="http://192.168.1.6/?test2" class="btn btn-info">Test 2</a>
+            
+        </div>
+        
+        <div class="footer">
+            Arduino
+        </div>
+    </div>
+    
+</body>
+</html>
